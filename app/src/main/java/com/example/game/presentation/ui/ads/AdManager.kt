1510a8744bbb310ee8a1a3a3651c42a25f517870
coding solution <codingsolution200@gmail.com>
2026-05-21 11:03:55 +0530

    Use live AdMob ad unit IDs

--- app/src/main/java/com/example/game/presentation/ui/ads/AdManager.kt
+++ app/src/main/java/com/example/game/presentation/ui/ads/AdManager.kt
@@ -19,7 +19,7 @@ import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
  */
 object AdManager {
 
-    private const val AD_UNIT_ID = "ca-app-pub-3940256099942544/1033173712" // Test ID
+    private const val AD_UNIT_ID = "ca-app-pub-8366632129348322/6580959052"
 
     private var interstitialAd: InterstitialAd? = null
     private var isLoading = false
